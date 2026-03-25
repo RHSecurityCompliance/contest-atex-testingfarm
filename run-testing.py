809870297b5756252dc0e595f397549c54885ead
Jiri Jaburek <comps@nomail.dom>
2026-03-25 15:07:10 +0100

    return back duration doubling (now that UEFI works)
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- run-testing.py
+++ run-testing.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 from atex.aggregator.jsonl import LZMAJSONLinesAggregator
 from atex.connection.local import LocalConnection
-from atex.executor.fmf import FMFExecutor, FMFTests
+from atex.executor.fmf import FMFExecutor, FMFTests, metadata
 from atex.orchestrator import adhoc
 from atex.provisioner.shvirt import SharedVirtProvisioner
 
@@ -203,6 +203,12 @@ with contextlib.ExitStack() as stack:
                 "arch": platform.machine(),
             },
         )
+
+        # adjust all tests to have twice their max duration
+        for data in fmf_tests.tests.values():
+            duration = data.get("duration", "5m")
+            secs = metadata.duration_to_seconds(duration)
+            data["duration"] = str(secs * 2)
 
         class PerStreamOrchestrator(
             ContestOrchestrator,
